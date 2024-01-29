b9a10b0fe6038fbddd1a4737f8e47f1c74c3993e
Martin H Berwanger <mberwanger@protonmail.com>
2024-01-29 15:49:53 -0500

    chore: rebrand (#5)

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -2,28 +2,28 @@ import {describe, expect, it} from '@jest/globals'
 import * as github from '../src/github'
 
 describe('getRelease', () => {
-  it('returns latest Datalift GitHub release', async () => {
+  it('returns latest Admiral GitHub release', async () => {
     const release = await github.getRelease('latest')
     expect(release).not.toBeNull()
     expect(release?.tag_name).not.toEqual('')
   })
 
-  it('returns v0.1.0 Datalift GitHub release', async () => {
+  it('returns v0.1.0 Admiral GitHub release', async () => {
     const release = await github.getRelease('v0.1.0')
     expect(release).not.toBeNull()
     expect(release?.tag_name).toEqual('v0.1.0')
   })
 
-  it('returns v0.1.0 for version range Datalift GitHub release', async () => {
+  it('returns v0.1.0 for version range Admiral GitHub release', async () => {
     const release = await github.getRelease('~> 0.1')
     expect(release).not.toBeNull()
     expect(release?.tag_name).toEqual('v0.1.0')
   })
 
-  it('unknown Datalift release', async () => {
+  it('unknown Admiral release', async () => {
     await expect(github.getRelease('foo')).rejects.toThrow(
       new Error(
-        'Cannot find Datalift release foo in https://releases.datalift.dev/datalift-cli/tags.json'
+        'Cannot find Admiral release foo in https://releases.datalift.dev/admiral/tags.json'
       )
     )
   })
