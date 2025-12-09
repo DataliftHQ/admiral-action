35c052f0e1cb8a9cd6593a7b20952595f3a4ca89
Martin H Berwanger <mberwanger@protonmail.com>
2025-12-09 09:43:38 -0500

    build: add docker bake configuration and simplify tests
    - Add docker-bake.hcl with build, lint, format, and test targets
    - Simplify admiral tests to focus on install functionality

--- __tests__/admiral.test.ts
+++ __tests__/admiral.test.ts
@@ -12,32 +12,9 @@ describe('install', () => {
     const bin = await admiral.install('latest');
     expect(fs.existsSync(bin)).toBe(true);
   }, 100000);
-});
 
-describe('getFilename', () => {
-  const app = rewire('../src/admiral');
-  const getFilename = app.__get__('getFilename');
-
-  console.log('reached');
-  const testCases = [
-    {q: ['x64', 'darwin', 'v0.1.0'], r: 'admiral-0.1.0_darwin_amd64.tar.gz'},
-    {q: ['arm64', 'darwin', 'v0.1.0'], r: 'admiral-0.1.0_darwin_arm64.tar.gz'},
-    {q: ['x64', 'linux', 'v0.1.0'], r: 'admiral-0.1.0_linux_amd64.tar.gz'},
-    {q: ['arm', 'linux', 'v0.1.0'], r: 'admiral-0.1.0_linux_arm.tar.gz'},
-    {q: ['arm64', 'linux', 'v0.1.0'], r: 'admiral-0.1.0_linux_arm64.tar.gz'},
-    {q: ['x32', 'linux', 'v0.1.0'], r: 'admiral-0.1.0_linux_i386.tar.gz'},
-    {q: ['x64', 'win32', 'v0.1.0'], r: 'admiral-0.1.0_windows_amd64.zip'},
-    {q: ['arm64', 'win32', 'v0.1.0'], r: 'admiral-0.1.0_windows_arm64.zip'},
-    {q: ['x32', 'win32', 'v0.1.0'], r: 'admiral-0.1.0_windows_i386.zip'}
-  ];
-
-  for (let i = 0; i < testCases.length; i++) {
-    const {q, r} = testCases[i];
-
-    it(`arch: ${q[0]}, platform: ${q[1]}, version: ${q[2]}`, async () => {
-      const actual: string = getFilename(...q);
-      expect(actual).not.toBeNull();
-      expect(actual).toEqual(r);
-    }, 100000);
-  }
+  it('acquires latest v0 version of Admiral', async () => {
+    const bin = await admiral.install('~> v0');
+    expect(fs.existsSync(bin)).toBe(true);
+  }, 100000);
 });
